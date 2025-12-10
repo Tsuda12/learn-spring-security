e1890f90c7efce1446ebe776607441d28b6264ad
Tsuda12 <ftsuda12@gmail.com>
2025-12-10 05:48:41 -0300

    feat: auth config

--- src/main/java/dev/tsuda/spring_security/controller/AuthController.java
+++ src/main/java/dev/tsuda/spring_security/controller/AuthController.java
@@ -9,6 +9,9 @@ import dev.tsuda.spring_security.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,19 +24,29 @@ public class AuthController {
 
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserRepository userRepository, PasswordEncoder encoder) {
+    public AuthController(UserRepository userRepository, PasswordEncoder encoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.encoder = encoder;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(
+                request.email(), request.password()
+        );
+
+        Authentication authentication = authenticationManager.authenticate(userAndPass);
+
         return null;
     }
 
     @PostMapping("/register")
     public ResponseEntity<RegisterUserResponse> register(@Valid @RequestBody RegisterUserRequest request) {
+
         User user = new User();
         user.setEmail(request.email());
         user.setName(request.name());
